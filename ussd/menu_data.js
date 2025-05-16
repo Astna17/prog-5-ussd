959da19909cd57fd24c53594c4a83900e35ad316
Astna17 <hei.antsa.17@gmail.com>
2025-05-16 12:47:30 +0300

    feat: create USSD menu

---
+++ ussd/menu_data.js
@@ -0,0 +1,49 @@
+const menuData = {
+  main: {
+    firstMainPage: {
+      options: {
+        '1': 'Mvola',
+        '2': 'Rappelle moi',
+        '3': 'SOS crédit',
+        '4': 'Services YAS',
+        '5': 'Promotion',
+        '6': 'Produits et divertissement',
+        '7': 'Banque et micro-finances',
+        '0': 'Page suivante'
+      },
+      nextPage: 2
+    },
+    SecondMainPage: {
+      options: {
+        '8': 'Mon identité',
+        '9': 'Configurer mon mobile',
+        '00':'Page précédente'
+      },
+      prevPage: 1
+    }
+  },
+  mvola: {
+    FirstMvolaPage: {
+      options: {
+        '1': 'Acheter crédit ou offre YAS',
+        '2': 'Transférer argent(vers toute destination)',
+        '3': 'Mvola crédit ou épargne',
+        '4': 'Retrait argent',
+        '#': 'Page suivante'
+      },
+      nextPage: 2
+    },
+    SecondMvolaPage: {
+      options: {
+        '5': 'Paiement facture & partenaires',
+        '6': 'Mon compte',
+        '7': 'Recevoir de l\'argent',
+        '8': 'Banque et micro-finances',
+        '*': 'Page précédente',
+        '**': 'Menu principal'
+      },
+      prevPage: 1
+    }
+  }
+};
+export default menuData;
