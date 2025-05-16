ed3070c9cd53ed2c8b20b7163e015b5d646d720f
Astna17 <hei.antsa.17@gmail.com>
2025-05-16 13:55:55 +0300

    fix: update ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,21 @@
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
-    extends: ["js/recommended"]
+    extends: ["js/recommended"],
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node 
+      },
+      parserOptions: {
+        ecmaVersion: 2021,
+      },
+    },
   },
-  {
-    files: ["**/*.js"],
-    languageOptions: { sourceType: "module" }
-  },
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { globals: globals.browser }
-  }
 ]);
